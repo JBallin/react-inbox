2197275ee9a8a63a6da4012b6be0f65853b4910e
JBallin <JBallin@users.noreply.github.com>
2018-07-24 10:26:34 -0700

    Add toolbar

---
+++ src/components/toolbar.js
@@ -0,0 +1,128 @@
+import React from 'react';
+
+export default props => {
+  if (props.selected === 'all') {
+    return (
+      <div className="row toolbar">
+        <div className="col-md-12">
+          <p className="pull-right">
+            <span className="badge badge">2</span>
+            unread messages
+          </p>
+
+          <button className="btn btn-default">
+            <i className="fa fa-check-square-o"></i>
+          </button>
+
+          <button className="btn btn-default">
+            Mark As Read
+          </button>
+
+          <button className="btn btn-default">
+            Mark As Unread
+          </button>
+
+          <select className="form-control label-select">
+            <option>Apply label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
+
+          <select className="form-control label-select">
+            <option>Remove label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
+
+          <button className="btn btn-default">
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </div>
+      </div>
+    )
+  } else if (props.selected === 'some') {
+    return (
+      <div className="row toolbar">
+        <div className="col-md-12">
+          <p className="pull-right">
+            <span className="badge badge">2</span>
+            unread messages
+          </p>
+
+          <button className="btn btn-default">
+            <i className="fa fa-minus-square-o"></i>
+          </button>
+
+          <button className="btn btn-default">
+            Mark As Read
+          </button>
+
+          <button className="btn btn-default">
+            Mark As Unread
+          </button>
+
+          <select className="form-control label-select">
+            <option>Apply label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
+
+          <select className="form-control label-select">
+            <option>Remove label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
+
+          <button className="btn btn-default">
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </div>
+      </div>
+    )
+  } else if (props.selected === 'none') {
+    return (
+      <div className="row toolbar">
+        <div className="col-md-12">
+          <p className="pull-right">
+            <span className="badge badge">2</span>
+            unread messages
+          </p>
+
+          <button className="btn btn-default">
+            <i className="fa fa-square-o"></i>
+          </button>
+
+          <button className="btn btn-default" disabled="disabled">
+            Mark As Read
+          </button>
+
+          <button className="btn btn-default" disabled="disabled">
+            Mark As Unread
+          </button>
+
+          <select className="form-control label-select" disabled="disabled">
+            <option>Apply label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
+
+          <select className="form-control label-select" disabled="disabled">
+            <option>Remove label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
+
+          <button className="btn btn-default" disabled="disabled">
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
