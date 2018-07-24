24ce060d34896a0aee3d7ca4d87e9f88811056c4
JBallin <JBallin@users.noreply.github.com>
2018-07-24 12:58:58 -0700

    Add Message component

---
+++ src/components/message.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Labels from './labels'
+
+export default props => {
+  return (
+    <div className={`row message ${props.message.read ? 'read' : 'unread'}`}>
+      <div className="col-xs-1">
+        <div className="row">
+          <div className="col-xs-2">
+            <input type="checkbox" />
+          </div>
+          <div className="col-xs-2">
+            <i className={`star fa fa-star${props.message.starred ? '' : '-o'}`}></i>
+          </div>
+        </div>
+      </div>
+      <div className="col-xs-11">
+        <Labels labels={props.message.labels} />
+        <a href="#">{props.message.subject}</a>
+      </div>
+    </div>
+  )
+}
