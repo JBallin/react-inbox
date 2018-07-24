a7a9860a22c21a0cf0c7c173d8c5781ba2389b8b
JBallin <JBallin@users.noreply.github.com>
2018-07-24 10:07:58 -0700

    Import components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import ToolBar from './components/toolbar'
+import MessageList from './components/messagelist'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <ToolBar />
+        <MessageList />
       </div>
     );
   }
