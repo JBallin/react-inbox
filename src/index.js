7a38a4ccc82eb5807fa6943ef4ea8ca97e60e58d
JBallin <JBallin@users.noreply.github.com>
2018-07-24 09:59:30 -0700

    Import bootstrap and font-awesome into index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
