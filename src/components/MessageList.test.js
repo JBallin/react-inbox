9ab40557241321a01d3394727e3cc0307a74f07e
JBallin <JBallin@users.noreply.github.com>
2018-07-24 16:03:29 -0700

    Add smoke test for MessageList

---
+++ src/components/MessageList.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MessageList from './MessageList';
+
+const messages = [
+  {
+    "id": 1,
+    "subject": "You can't input the protocol without calculating the mobile RSS protocol!",
+    "read": false,
+    "starred": true,
+    "labels": ["dev", "personal"]
+  },
+  {
+    "id": 2,
+    "subject": "connecting the system won't do anything, we need to input the mobile AI panel!",
+    "read": false,
+    "starred": false,
+    "selected": true,
+    "labels": []
+  }
+]
+
+describe('MessageList', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<MessageList messages={messages}/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+})
