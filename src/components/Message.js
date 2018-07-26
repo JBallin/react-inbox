7970e49900d76dc3c424a2190803952a41445850
JBallin <JBallin@users.noreply.github.com>
2018-07-25 21:08:30 -0700

    Use helper functions getRead and getStarred

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Labels from './Labels'
 
+const getRead = message => message.read ? 'read' : 'unread';
+const getStarred = message => message.starred ? '' : '-o';
+
 export default ({ message }) => {
   return (
-    <div className={`row message ${props.message.read ? 'read' : 'unread'}`}>
+    <div className={`row message ${getRead(message)}`}>
       <div className="col-xs-1">
         <div className="row">
           <div className="col-xs-2">
             <input type="checkbox" />
           </div>
           <div className="col-xs-2">
-            <i className={`star fa fa-star${props.message.starred ? '' : '-o'}`}></i>
+            <i className={`star fa fa-star${getStarred(message)}`}></i>
           </div>
         </div>
       </div>
