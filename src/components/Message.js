10337d4ea08728acbbc14e9f810b62d0566d00c5
JBallin <JBallin@users.noreply.github.com>
2018-07-31 18:58:52 -0700

    Refactor Message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import Labels from './Labels'
 
-const getRead = message => message.read ? 'read' : 'unread';
-const getStarred = message => message.starred ? '' : '-o';
-const getSelected = message => message.selected ? 'selected' : '';
-
 export default ({ message, toggleSelected, toggleStarred }) => {
+  const readClass = message.read ? 'read' : 'unread';
+  const starClass = `star fa fa-star${message.starred ? '' : '-o' }`
+  const selectedClass = message.selected ? 'selected' : '';
+
   return (
-    <div className={`row message ${getRead(message)} ${getSelected(message)}`}>
+    <div className={`row message ${readClass} ${selectedClass}`}>
       <div className="col-xs-1">
         <div className="row">
           <div className="col-xs-2">
-            <input type="checkbox" checked={Boolean(message.selected)} onChange={e => toggleSelected(message, e)}/>
+            <input type="checkbox" checked={!!message.selected} onChange={() => toggleSelected(message)}/>
           </div>
           <div className="col-xs-2">
-            <i className={`star fa fa-star${getStarred(message)}`} onClick={e => toggleStarred(message, e)}></i>
+            <i className={starClass} onClick={() => toggleStarred(message)}></i>
           </div>
         </div>
       </div>
