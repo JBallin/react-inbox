56912af817f6c69ec980f2e61397a58acf64608c
JBallin <JBallin@users.noreply.github.com>
2018-08-08 13:47:06 -0700

    Seed messages in ToolBar.test.js

--- src/components/ToolBar.test.js
+++ src/components/ToolBar.test.js
@@ -2,10 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ToolBar from './ToolBar';
 
+const messages = [
+  {
+    "id": 1,
+    "subject": "You can't input the protocol without calculating the mobile RSS protocol!",
+    "read": false,
+    "starred": true,
+    "labels": ["dev", "personal"]
+  },
+  {
+    "id": 2,
+    "subject": "connecting the system won't do anything, we need to input the mobile AI panel!",
+    "read": false,
+    "starred": false,
+    "selected": true,
+    "labels": []
+  }
+]
+
 describe('ToolBar', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<ToolBar selected='none' messages={[]} />, div);
+    ReactDOM.render(<ToolBar selected='none' messages={messages} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 })
