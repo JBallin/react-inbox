e1e778c7eabc7498bd44036d35dd159da184539f
JBallin <JBallin@users.noreply.github.com>
2018-11-02 14:48:11 -0700

    Refactor Message elements into vars

--- src/components/Message.js
+++ src/components/Message.js
@@ -11,25 +11,37 @@ const Message = ({ message, isSelected, toggleSelect, toggleStar }) => {
     toggleStar([message.id]);
   }
 
+  const checkBox = (
+    <div className="col-xs-2">
+      <input
+        type="checkbox"
+        checked={ isSelected }
+        onChange={() => toggleSelect(message.id)}/>
+    </div>
+  );
+
+  const star = (
+    <div className="col-xs-2">
+      <i className={starClass} onClick={starMessage}></i>
+    </div>
+  );
+
+  const labelsAndSubject = (
+    <div className="col-xs-11">
+      <Labels labels={message.labels} />
+      <a>{message.subject}</a>
+    </div>
+  );
+
   return (
     <div className={`row message ${readClass} ${selectedClass}`}>
       <div className="col-xs-1">
         <div className="row">
-          <div className="col-xs-2">
-            <input
-              type="checkbox"
-              checked={ isSelected }
-              onChange={() => toggleSelect(message.id)}/>
-          </div>
-          <div className="col-xs-2">
-            <i className={starClass} onClick={starMessage}></i>
-          </div>
+          { checkBox }
+          { star }
         </div>
       </div>
-      <div className="col-xs-11">
-        <Labels labels={message.labels} />
-        <a href={`#${message.id}`}>{message.subject}</a>
-      </div>
+      { labelsAndSubject }
     </div>
   )
 }
